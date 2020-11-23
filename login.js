94e4b83dffc48aa1c41a845de68a5af868565b96
liviya-vincent <74824800+liviya-vincent@users.noreply.github.com>
2020-11-23 23:20:28 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,52 @@
+
+
+// function ValidateEmail(mail) 
+// {
+//  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+//   {
+//     return (true)
+//   }
+//     alert("You have entered an invalid email address!")
+//     return (false)
+// }
+
+// let email= document.getElementById("email");
+// let pwd= document.getElementById("pwd");
+// let error=document.getElementById("error");
+
+// function Validate(){
+//     if (email.value==""){
+//         alert("email can't be empty");
+//         return false;
+//     }
+//     else if(pwd.value==""){
+//         alert("password can't be blank")
+//         return false;
+
+//     }
+
+    // else if(pwd.value.length <=8) {
+    //     alert("password is too short");
+    //     pwd.style.border=2px solid red;
+    //     return false;
+
+    // }
+    // else{
+    //     return true;
+    // }
+
+
+//     var regexp = /^([a-zA-Z0-9\.\-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/
+
+//    if (regexp.test(email.value)){
+//        Error.innerHTML="valid";
+//        error.style.color="green";
+//        return true;
+
+//    }
+//    else{
+//        error.innerHTML="invalid";
+//        error.style.color="red";
+//        return false;
+//    } 
+// }
